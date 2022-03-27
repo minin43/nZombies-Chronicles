af031fab1fceafc57a1d56e2d328924ce49dab31
Ethorbit <Ethorbit@protonmail.com>
2022-03-27 13:09:01 -0700

    Add 'Special' to Zombie Spawn Creator

--- addons/nzombies-chronicles/gamemodes/nzombies/gamemode/enemies/sh_spawner.lua
+++ addons/nzombies-chronicles/gamemodes/nzombies/gamemode/enemies/sh_spawner.lua
@@ -3,21 +3,21 @@ Spawner = Spawner or {}
 if Spawner != nil then
     function Spawner:GetAll()
         local spawners = {}
-    
+
         for _,ent in pairs(ents.GetAll()) do
             if ent.NZSpawner then
                 spawners[#spawners + 1] = ent
             end
         end
-    
+
         return spawners
     end
 
     function Spawner:GetAliasAndClasses(special_alias) -- Spawners don't have aliases, we just use for stuff like the Tool Menu
-        local alias_and_classes = {}
-        
+        local alias_and_classes = {["Special"] = "nz_spawn_zombie_special"}
+
         for _,scripted_tbl in pairs(scripted_ents.GetList()) do
-            local tbl = scripted_tbl.t  
+            local tbl = scripted_tbl.t
             if tbl and scripted_tbl.Base == "nz_spawner_base" then
                 if !special_alias then
                     alias_and_classes[tbl.PrintName or "Undefined"] = tbl.ClassName
