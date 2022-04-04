a9b85db2f45e22e830a3da30d961b5fb5cebe30e
Ethorbit <Ethorbit@protonmail.com>
2022-04-03 17:00:54 -0700

    Fix Easter Egg preload/play issues

--- addons/nzombies-chronicles/gamemodes/nzombies/gamemode/easter_eggs/cl_ee.lua
+++ addons/nzombies-chronicles/gamemodes/nzombies/gamemode/easter_eggs/cl_ee.lua
@@ -14,41 +14,71 @@ end)
 
 EasterEggData = EasterEggData or {}
 EasterEggData.AudioChannel = EasterEggData.AudioChannel or nil
+EasterEggData.PreloadedSong = EasterEggData.PreloadedSong or nil
 
 net.Receive("EasterEggSong", function()
 	EasterEggData.PlaySong()
 end)
-	
+
 net.Receive("EasterEggSongPreload", function()
 	timer.Simple(1, function()
 		EasterEggData.ParseSong()
 	end)
 end)
-	
+
 net.Receive("EasterEggSongStop", function()
 	EasterEggData.StopSong()
 end)
-	
+
+local function play_song(url, flags) -- Added by Ethorbit to make it easier and more reliable to PlayURL, with error handling and proper playback
+	local isPreloading = string.find(flags, "noplay")
+	if isPreloading then
+		if EasterEggData.PreloadedSong == url then return end -- We already did this..
+		EasterEggData.PreloadedSong = url
+	end
+
+	if !isPreloading and IsValid(EasterEggData.AudioChannel) then
+		print("Playing easter egg song!")
+		EasterEggData.AudioChannel:Play()
+	return end
+
+	sound.PlayURL(url, flags, function(channel, errorID, errorName)
+		EasterEggData.AudioChannel = channel
+
+		if !isPreloading then
+			print("Easter egg song was not preloaded, will play through streaming.")
+			print(url)
+
+			if errorID then
+				chat.AddText("[nZombies] An error occurred when trying to play the Easter Egg. - ", errorID, errorName)
+			return end
+
+			if IsValid(channel) then
+				channel:Play()
+			end
+		else
+			if !errorID then
+				print("Successfully preloaded easter egg song")
+			else
+				print("Failed to preload song - ", errorID, errorName)
+			end
+		end
+	end)
+end
+
 function EasterEggData.ParseSong(play)
 	if !GetConVar("nz_eastereggsongs"):GetBool() then
-		print("Prevented loading the Easter Egg song because you have nz_eastereggsongs to 0")
-		return
-	end
+		print("Prevented loading the Easter Egg song because you have nz_eastereggsongs set to 0.")
+	return end
 
 	if !nzMapping.Settings.eeurl then return end
 	local url = nzMapping.Settings.eeurl
 	--local url = string.lower(nzMapping.Settings.eeurl) -- This is not only unnecessary but also breaks Google Drive URLs..
 	if url == nil or url == "" then return end
-		
-	--local googledrive = string.find(url, "docs.google.com/")
-	local soundcloud = string.find(url, "soundcloud.com/")
-	-- if !soundcloud then --and !googledrive then 
 
-	-- 	--print("Easter Egg Song currently only supports Soundcloud or Google Drive.") 
-	-- 	--print("For Google Drive you MUST format URLs like so: http://docs.google.com/uc?export=open&id=<id of url>")
-	-- return end
-	
-	if (soundcloud) then
+	local soundcloud = string.find(url, "soundcloud.com/")
+
+	if (soundcloud) then -- TODO: fix client_id issue. Soundcloud deprecated it in July 2021 and as of writing this they are not yet accepting new API applications.. /Ethorbit
 		http.Fetch( "http://api.soundcloud.com/resolve?url="..url.."&client_id=d8e0407577f7fc8475978904ef89b1f7",
 			function( body, len, headers, code )
 				if body then
@@ -66,50 +96,40 @@ function EasterEggData.ParseSong(play)
 						print("This Soundcloud song does not have allow streaming.")
 					end
 				return end
-			end, 
+			end,
 			function( error )
 				Error( "Failed to fetch song! Error: " .. error )
 			end )
 	else
 		if play then
 			EasterEggData.PlaySong(url)
-		--else
-		--	EasterEggData.PreloadSong(url)
+		else
+			EasterEggData.PreloadSong(url)
 		end
 	end
 end
-	
-function EasterEggData.PlaySong(url)
+
+function EasterEggData.PlaySong(url) -- Modified by Ethorbit, moved most functionality into the local helper function 'play_song' above
 	EasterEggData.StopSong()
-	
+
 	if !GetConVar("nz_eastereggsongs"):GetBool() then
-		print("Prevented loading the Easter Egg song because you have nz_eastereggsongs to 0")
+		print("Prevented playing the Easter Egg song because you have nz_eastereggsongs set to 0.")
 		return
 	end
 
-	-- We have a preloaded channel
-	if IsValid(EasterEggData.AudioChannel) then
-		EasterEggData.AudioChannel:Play()
-		print("Playing easter egg song!")
-	-- We need to instantly play the given link
-	elseif url then
-		--print("Playing!")
-		sound.PlayURL( url, "", function(channel) EasterEggData.AudioChannel = channel end)
-		print("Easter egg song was not preloaded, will play through streaming.")
-		print(url)
-	-- No link and no preload, parse the link and loopback to above
-	else
+	if !url then
 		EasterEggData.ParseSong(true)
-	end
+	return end
+
+	play_song(url, "")
 end
-	
+
 function EasterEggData.StopSong()
 	if IsValid(EasterEggData.AudioChannel) then
 		EasterEggData.AudioChannel:Stop()
 	end
 end
-	
-function EasterEggData.PreloadSong(song)
-	sound.PlayURL( song, "noplay noblock", function(channel) EasterEggData.AudioChannel = channel end)
-	print("Successfully preloaded easter egg song")
+
+function EasterEggData.PreloadSong(song) -- Modified by Ethorbit  ^^^^^^^
+	play_song(song, "noplay noblock")
 end
