bf13a0072e6e240a39902bf4965fb05987b401a4
Ethorbit <Ethorbit@protonmail.com>
2022-04-13 03:13:06 -0700

    Remove unused tbl

--- addons/nzombies-chronicles/gamemodes/nzombies/gamemode/spectator/sv_player.lua
+++ addons/nzombies-chronicles/gamemodes/nzombies/gamemode/spectator/sv_player.lua
@@ -16,15 +16,8 @@ function plyMeta:SetSpectator()
 	self:SetSpectatingID( 1 )
 end
 
-local spectate_ent_blacklist = {
-	["predicted_viewmodel"] = true,
-	["manipulate_bone"] = true,
-	["hl2mp_ragdoll"] = true,
-}
-
 function plyMeta:SpectateClosestEntity()
 	local closestEnt = nzMisc:GetClosestEntityToPosition(self:GetPos(), function(ent)
-		--!spectate_ent_blacklist[ent:GetClass()]
 		return ent != self and ent:GetParent() != self and ent:IsInWorld() and (!ent:IsPlayer() or ent:Team() == TEAM_PLAYERS)
 	end)
 
