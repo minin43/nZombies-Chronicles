7a27ef13c93da8bfd40919ff9aa739745108dc37
Ethorbit <Ethorbit@protonmail.com>
2022-05-03 07:16:18 -0700

    Update admin settings

--- addons/nzombies-chronicles/gamemodes/nzombies/gamemode/mainmenu/sh_adminsettings.lua
+++ addons/nzombies-chronicles/gamemodes/nzombies/gamemode/mainmenu/sh_adminsettings.lua
@@ -130,28 +130,78 @@ if CLIENT then
         controlPanel:Dock(TOP)
         controlPanel:SetHeight(30)
 
+        local controlPanelLeft = vgui.Create("DPanel", controlPanel)
+        controlPanelLeft:Dock(FILL)
+
+        local controlPanelRight = vgui.Create("DPanel", controlPanel)
+        controlPanelRight:Dock(RIGHT)
+        controlPanelRight:SetWide(300)
+
+        local controlPanelRightScroller = vgui.Create("DHorizontalScroller", controlPanelRight)
+        controlPanelRightScroller:Dock(FILL)
+        controlPanelRightScroller:SetOverlap(-4)
+
         local controlPanelSpacer = vgui.Create("DPanel", mapFilterPanel)
         controlPanelSpacer:Dock(TOP)
         controlPanelSpacer:SetHeight(5)
 
-        local configOrMapButton = vgui.Create("DButton", controlPanel)
+        local configOrMapButton = vgui.Create("DButton", controlPanelLeft)
         configOrMapButton:Dock(LEFT)
-        configOrMapButton:SetWide(178)
+        configOrMapButton:SetWide(140)
 
-        local whiteListAllButton = vgui.Create("DButton", controlPanel)
+        local whiteListButton = vgui.Create("DButton")
+        whiteListButton:Dock(LEFT)
+        whiteListButton:SetWide(85)
+        whiteListButton:SetText("Whitelist")
+        controlPanelRightScroller:AddPanel(whiteListButton)
+
+        local blackListButton = vgui.Create("DButton")
+        blackListButton:Dock(LEFT)
+        blackListButton:SetWide(85)
+        blackListButton:SetText("Blacklist")
+        controlPanelRightScroller:AddPanel(blackListButton)
+
+        local whiteListAllButton = vgui.Create("DButton")
         whiteListAllButton:Dock(LEFT)
-        whiteListAllButton:SetWide(90)
+        whiteListAllButton:SetWide(85)
         whiteListAllButton:SetText("Whitelist All")
+        controlPanelRightScroller:AddPanel(whiteListAllButton)
 
-        local blackListAllButton = vgui.Create("DButton", controlPanel)
+        whiteListButton.Think = function()
+            if blacklistedCount > 0 and !whiteListButton:GetDisabled() then
+                whiteListButton:SetDisabled(true)
+                whiteListAllButton:SetDisabled(true)
+            end
+
+            if whiteListButton:GetDisabled() and blacklistedCount <= 0 then
+                whiteListButton:SetDisabled(false)
+                whiteListAllButton:SetDisabled(false)
+            end
+        end
+
+        local blackListAllButton = vgui.Create("DButton")
         blackListAllButton:Dock(LEFT)
-        blackListAllButton:SetWide(90)
+        blackListAllButton:SetWide(85)
         blackListAllButton:SetText("Blacklist All")
+        controlPanelRightScroller:AddPanel(blackListAllButton)
 
-        local resetButton = vgui.Create("DButton", controlPanel)
+        blackListButton.Think = function()
+            if whitelistedCount > 0 and !blackListButton:GetDisabled() then
+                blackListButton:SetDisabled(true)
+                blackListAllButton:SetDisabled(true)
+            end
+
+            if blackListButton:GetDisabled() and whitelistedCount <= 0 then
+                blackListButton:SetDisabled(false)
+                blackListAllButton:SetDisabled(false)
+            end
+        end
+
+        local resetButton = vgui.Create("DButton")
         resetButton:Dock(LEFT)
-        resetButton:SetWide(90)
+        resetButton:SetWide(85)
         resetButton:SetText("Clear")
+        controlPanelRightScroller:AddPanel(resetButton)
 
         local filter_list = vgui.Create("DListView", mapFilterPanel)
         filter_list:Dock(FILL)
@@ -161,14 +211,38 @@ if CLIENT then
         local unlisted_map_column = filter_list:AddColumn("Map")
         local unlisted_config_column = filter_list:AddColumn("Config")
         local blacklisted_map_or_config_column = filter_list:AddColumn("Blacklisted")
-        whitelisted_map_or_config_column:SetMinWidth(90)
+        whitelisted_map_or_config_column:SetFixedWidth(90)
         unlisted_map_column:SetMinWidth(90)
         unlisted_config_column:SetMinWidth(90)
-        blacklisted_map_or_config_column:SetMinWidth(90)
+        blacklisted_map_or_config_column:SetFixedWidth(90)
+
+        filter_list.Think = function()
+            local whitelistWide = whitelisted_map_or_config_column:GetWide()
+            if whitelistWide == 0 and blacklistedCount <= 0 then
+                whitelisted_map_or_config_column:SetFixedWidth(90)
+                unlisted_map_column:SetWidth(100)
+            end
+
+            if whitelistWide == 90 and blacklistedCount > 0 then
+                whitelisted_map_or_config_column:SetFixedWidth(0)
+                unlisted_map_column:SetWidth(100)
+            end
+
+            local blacklistWide = blacklisted_map_or_config_column:GetWide()
+            if blacklistWide == 0 and whitelistedCount <= 0 then
+                blacklisted_map_or_config_column:SetFixedWidth(90)
+                unlisted_map_column:SetWidth(100)
+            end
+
+            if blacklistWide == 90 and whitelistedCount > 0 then
+                blacklisted_map_or_config_column:SetFixedWidth(0)
+                unlisted_map_column:SetWidth(100)
+            end
+        end
 
         local filter_applied_text = "yes" -- What to show under the Whitelisted/Blacklisted category when said filter is set
 
-        local function switch_to_configs_or_maps()
+        local function switch_to_configs_or_maps(skipWarning)
             local function go_on()
                 filter_list:Clear()
                 unsavedChanges = false
@@ -176,8 +250,8 @@ if CLIENT then
                 blacklistedCount = 0
 
                 if isEditingMaps then
-                    whitelisted_map_or_config_column:SetFixedWidth(90)
-                    blacklisted_map_or_config_column:SetFixedWidth(90)
+                    --whitelisted_map_or_config_column:SetFixedWidth(90)
+                    --blacklisted_map_or_config_column:SetFixedWidth(90)
 
                     configOrMapButton:SetText("Edit Configs")
                     unlisted_config_column:SetWidth(0)
@@ -187,8 +261,8 @@ if CLIENT then
                     net.Start("NZ_AdminSettings_NeedMaps")
                     net.SendToServer()
                 else
-                    whitelisted_map_or_config_column:SetFixedWidth(70)
-                    blacklisted_map_or_config_column:SetFixedWidth(70)
+                    --whitelisted_map_or_config_column:SetFixedWidth(70)
+                    --blacklisted_map_or_config_column:SetFixedWidth(70)
 
                     unlisted_config_column:SetWidth(100)
                     unlisted_map_column:SetWidth(100)
@@ -200,7 +274,7 @@ if CLIENT then
                 end
             end
 
-            if unsavedChanges then
+            if unsavedChanges and !skipWarning then
                 mapFilterPanel:ShowConfirmationMenu("Switch editor?", "You have unsaved changes, are you sure you want to discard them?", function(val)
                     if val then
                         go_on()
@@ -290,7 +364,7 @@ if CLIENT then
             mapFilterPanel:ShowConfirmationMenu("Whitelist All?", "Are you sure you want to whitelist ALL items?", function(val)
                 if val then
                     unsavedChanges = true
-                    whitelistedCount = 0
+                    --whitelistedCount = 0
 
                     --for _,line in pairs()
                 end
@@ -301,7 +375,7 @@ if CLIENT then
             mapFilterPanel:ShowConfirmationMenu("Blacklist All?", "Are you sure you want to blacklist ALL items?", function(val)
                 if val then
                     unsavedChanges = true
-                    blacklistedCount = 0
+                    --blacklistedCount = 0
                 end
             end)
         end
@@ -309,6 +383,7 @@ if CLIENT then
         resetButton.DoClick = function()
             mapFilterPanel:ShowConfirmationMenu("Clear Filters?", "Are you sure?\n\nThis will remove ALL whitelist or blacklist filters EVER applied.", function(val)
                 if val then
+                    switch_to_configs_or_maps(true)
                     unsavedChanges = true
                 end
             end)
